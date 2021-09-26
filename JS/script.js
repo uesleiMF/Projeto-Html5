86feba1f7dcd353dccff4598d747ff9730967653
Ueslei de Moraes <60453278+uesleiMF@users.noreply.github.com>
2021-09-26 15:01:49 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 var imagens = [
-    "https://grupobracosabertos.com.br/wp-content/uploads/2020/03/Como-ajudar-uma-pessoa-com-depressao-768x512.jpg",
-    "https://i1.wp.com/hospitalsantamonica.com.br/wp-content/uploads/2019/08/depressao-doencas-cronicas.jpg?w=1024&ssl=1",
-    "https://exame.com/wp-content/uploads/2016/09/size_960_16_9_depressao_tristeza6.jpg?quality=70&strip=info",
-    "https://lh5.googleusercontent.com/proxy/G4eg3lND-ciDip9X-o-BQFT6qP3g4AwUAV24UUukAH3GzjA9B6S3pHDI-7zXaqkuyjemHCbJsoPQ50dJni-SgLGFv_9MDnqHNHRMZofAsH82neFQs7b2bwZ6V-2ScrBa8djYdHOsY-OIH28PXbcejOG9m2DkGA=w1200-h630-p-k-no-nu" 
+    "https://static8.depositphotos.com/1526816/1071/v/600/depositphotos_10714227-stock-illustration-sad-man.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAiqMfF1YxaimWzB2SevhC2SNsJN7qyupFpxroHIUn2rQQ8Mb5v85n4EQp3N3fK-kOn2U&usqp=CAU",
+    "https://i.pinimg.com/236x/2e/af/20/2eaf20e262d35c8a6fbc60dc0ebedf22.jpg",
+    "https://i1.wp.com/emotioncard.com.br/wp-content/uploads/2016/04/c3aa55c518041fdcd1eae5304eeb9b49-sad-art-dark-quotes.jpg?resize=236%2C336&ssl=1" 
  ];
   
  function trocar(){
@@ -13,9 +13,9 @@ var imagens = [
  } 
 
  var fotos = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw4c0ciXnjzsWXOj-tfZtoRYNIt8Dpi7z-IGCILv3E2UEnMu3nf2odieqZ9SoyAg5slCs&usqp=CAU",
-    "http://1.bp.blogspot.com/-BmaB4zl3x-A/UqSPIZ10PbI/AAAAAAAABJo/A_-QTAMszFc/s640/obra_salvifica.jpg",
-    "https://4.bp.blogspot.com/-KqynROmf9DY/VJc7gJ-r3lI/AAAAAAAACKQ/CmX9X_NmsPU/s1600/levantar%2Bdas%2Bm%C3%A3os.jpg",
+    "https://freepikpsd.com/media/2019/10/pessoa-feliz-desenho-png.png",
+     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT8YqGdPY9x-12cik0pUyB2uG_b1LJc-PGCA&usqp=CAU",
+    "https://img1.gratispng.com/20180823/rvy/kisspng-businessperson-company-small-business-paper-organi-telegram-amp-q-5b7f524394be85.7261299615350707876093.jpg",
     "https://1.bp.blogspot.com/-Zy373Hbq0Yw/WFBw6GV9NeI/AAAAAAAAAwg/0izRl9wlBa8jaVlG8ca4qMznhW7EHb8QQCLcB/s1600/atitude-mulher-feliz.jpg"
  ];
   
@@ -25,6 +25,9 @@ var imagens = [
     var img_idx = fotos.indexOf(img_src);
     img.src = fotos[ img_idx == fotos.length-1 ? 0 : img_idx+1 ];
  } 
+ 
+
+ 
  
 
  
